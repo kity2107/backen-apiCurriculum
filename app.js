fef909e4ba9ff28f8ce51ce3c5742ace7c7ccc9e
Cristian Panontini <kity2107@gmail.com>
2022-05-23 19:01:34 -0300

    se finalizo slack

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+const morganBody = require('morgan-body');
+const loggerStream = require('./utils/handleLogger');
+
 const dbConnect = require('./config/mongo');
 const app = express();
 
@@ -8,6 +12,14 @@ app.use(cors());
 //capturo el json q envia el body
 app.use(express.json());
 app.use(express.static('storage'));
+
+morganBody(app, {
+  noColors: true,
+  stream: loggerStream,
+  skip: function (req, res) {
+    return res.statusCode < 400;
+  },
+});
 
 const port = process.env.PORT || 3000;
 
